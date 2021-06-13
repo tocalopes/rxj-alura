5e9c6499eb83ebb59b153a2929ceb5c6d3d07b86
gabriel <gabriellopes112009@gmail.com>
2021-06-13 17:30:34 -0300

    uso do map, pluck e tap do rxjs para geraçõa da lista de acoes

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { AcoesService } from './acoes.service';
+import { Acoes } from './modelo/acoes';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
+export class AcoesComponent implements OnInit{
   acoesInput = new FormControl();
+  acoes: Acoes;
 
-  constructor() {}
+
+  constructor(private acoesService: AcoesService) {}
+
+  ngOnInit(): void {
+    this.acoesService.getAcoes().subscribe((acoes) => {
+      this.acoes = acoes;
+    })
+  }
+
 }
--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {map, pluck, tap} from 'rxjs/operators';
+import { Acao } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,25 @@ export class AcoesService {
   constructor(private http: HttpClient) { }
 
   getAcoes(){
-    return this.http.get<any>('http://localhost:3000/acoes');
+    return this.http.get<any>('http://localhost:3000/acoes')
+      .pipe(
+        tap((valor) => console.log(valor)),
+        pluck('payload'),
+        //pluck extrai um atributo direto
+        // map((api) => api.payload),
+        map(acoes => acoes.sort((acaoA,acaoB) => this.ordenaPorCodigo(acaoA,acaoB)))
+      );
+    }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if(acaoA.codigo > acaoB.codigo){
+      return 1;
+    }
+    if(acaoA.codigo < acaoB.codigo){
+      return -1;
+    }
+    return 0;
+
   }
 
 }
